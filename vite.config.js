87eb42e474fca9c38e675600579957e58edb79f6
tee <aderetioluwatoyin@gmail.com>
2023-09-21 22:43:16 +0100

    newer commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/HNG-stage-3-Gallery/"
+  base:"/HNG-stage-3-Gallery/",
   plugins: [react()],
   build: {
     rollupOptions: {
